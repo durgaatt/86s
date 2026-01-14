32b5cdba4f3ce150a3544cd0c78f218ada67519a
devopsaws644 <devopsaws644@gmail.com>
2026-01-14 15:41:31 +0530

    VPC

---
+++ terraform_roboshop_dev_infra/00-vpc/main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+    source = "git::https://github.com/durgaatt/86s.git//terraform-vpc-module?ref=main"
+    # VPC
+    tenancy = var.tenancy
+    vpc_cidr = var.cidr_ips
+    project = var.project
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    # public subnets
+    public-cidr-blocks = var.public-cidr-blocks
+    vpc_pub_subnet_tags = var.vpc_pub_subnet_tags
+
+    # private subnets
+    private-cidr-blocks = var.private-cidr-blocks
+
+    # database subnets
+    database-cidr-blocks = var.database-cidr-blocks
+
+}
---
+++ terraform_roboshop_dev_infra/00-vpc/variables.tf
@@ -0,0 +1,66 @@
+variable "cidr_ips" {
+   default = "10.10.0.0/16"
+}
+
+variable "tenancy" {
+  default = "default"
+}
+
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {
+        Name = "nsp"
+        DontDelete = "true"
+    }
+}
+
+variable "vpc_pub_subnet_tags" {
+    type = map
+    default = {
+        Name = "nsp-pub-subnet"
+        DontDelete = "true"
+    }
+}
+
+variable "vpc_priv_subnet_tags" {
+    type = map
+    default = {
+        Name = "nsp-priv-subnet"
+        DontDelete = "true"
+    }
+}
+
+variable "vpc_db_subnet_tags" {
+    type = map
+    default = {
+        Name = "nsp-db-subnet"
+        DontDelete = "true"
+    }
+}
+
+variable "public-cidr-blocks" {
+  type = list
+  default = ["10.10.10.0/24","10.10.11.0/24"]
+}
+
+variable "private-cidr-blocks" {
+  type = list
+  default = ["10.10.20.0/24","10.10.21.0/24"]
+}
+
+variable "database-cidr-blocks" {
+  type = list
+  default = ["10.10.30.0/24","10.10.31.0/24"]
+}
+
+variable "dest-cidr" {
+  default = "0.0.0.0/0"
+}
