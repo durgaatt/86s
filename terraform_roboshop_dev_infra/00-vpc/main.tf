715ff248a001be21e6f27b896a7759f558b77f45
devopsaws644 <devopsaws644@gmail.com>
2026-01-14 15:47:27 +0530

    VPC

--- terraform_roboshop_dev_infra/00-vpc/main.tf
+++ terraform_roboshop_dev_infra/00-vpc/main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
     source = "git::https://github.com/durgaatt/86s.git//terraform-vpc-module?ref=main"
     # VPC
+    cidr_ips = var.cidr_ips
     tenancy = var.tenancy
     vpc_cidr = var.cidr_ips
     project = var.project
@@ -9,7 +10,7 @@ module "vpc" {
 
     # public subnets
     public-cidr-blocks = var.public-cidr-blocks
-    vpc_pub_subnet_tags = var.vpc_pub_subnet_tags
+    # vpc_pub_subnet_tags = var.vpc_pub_subnet_tags
 
     # private subnets
     private-cidr-blocks = var.private-cidr-blocks
@@ -17,4 +18,5 @@ module "vpc" {
     # database subnets
     database-cidr-blocks = var.database-cidr-blocks
 
+    dest-cidr = var.dest-cidr
 }
