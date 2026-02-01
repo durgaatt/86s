0e417840232339c2c46688d89fc8501cb27863a7
devopsaws644 <devopsaws644@gmail.com>
2026-02-01 18:33:56 +0530

    Q&A

--- terraform-vpc-module/vpc-resource.tf
+++ terraform-vpc-module/vpc-resource.tf
@@ -67,18 +67,6 @@ resource "aws_route_table" "public" {
   )
 }
 
-resource "aws_route" "public" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = var.dest_cidr
-  gateway_id = aws_internet_gateway.gw.id
-}
-
-resource "aws_route_table_association" "public" {
-  count=length(var.public-cidr-blocks)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
@@ -98,7 +86,6 @@ resource "aws_route_table" "database" {
   }
   )
 }
-
 
 resource "aws_eip" "nat" {
   domain   = "vpc"
@@ -126,6 +113,12 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.gw]
 }
 
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = var.dest_cidr
+  gateway_id = aws_internet_gateway.gw.id
+}
+
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = var.dest_cidr
@@ -138,6 +131,11 @@ resource "aws_route" "database" {
   gateway_id = aws_nat_gateway.nat.id
 }
 
+resource "aws_route_table_association" "public" {
+  count=length(var.public-cidr-blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
 resource "aws_route_table_association" "private" {
    count = length(var.private-cidr-blocks)
